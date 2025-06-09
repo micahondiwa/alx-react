6a5f72c3957668ea9c3c9d5f5169987a5e8edef3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-09 13:03:31 +0300

    Task_01 dashbord

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import logo from './logo.jpg';
+
+function App() {
+  return (
+    <>
+    <section>
+      <div className="App-header">
+      <img src={logo} alt="ALX Logo"/>
+      <h1>School dashboard</h1>
+    </div>
+    </section>
+
+    <hr className='Horizontal-rule'></hr>
+
+    <section>
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+     </div>
+     </section>
+
+     <hr className='Horizontal-rule'></hr>
+
+    <section>  
+    <div className="App-footer">
+        <p>Copyright 2020 - ALX</p>
+      </div> 
+    </section>
+    </>
+
+      );
+}
+
+export default App;
