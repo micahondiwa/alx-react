0f2cf40ad29bb82e7bf08e279651eaba77066fd0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-10 16:13:14 +0300

    Utils.js tests

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -2,4 +2,9 @@
 
 import {getFullYear, getFooterCopy, getLatestNotification} from './utils'
 
-describe('utils.js tests', () =>)
+describe('utils.js tests', () => {
+    testUtils('getFullYear returns the current year', () => {
+        const currentYear = new Date().getFullYear();
+        
+    })
+})
