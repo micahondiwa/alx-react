067efafda0fb707c39adccd21ffcbbe929a65bbb
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-10 10:42:15 +0300

    Button element

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -5,6 +5,7 @@ export function Notifications() {
     return (
         <div className='Notifications'>
             <p>Here is the list of notifications</p>
+            <button> </button>
         </div>
     )
 }
