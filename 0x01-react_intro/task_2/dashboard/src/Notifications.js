5aa80fa158a68ec4978a33310f48a7a167b78ef4
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-10 13:19:37 +0300

    Margin left

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -2,10 +2,10 @@ import './Notifications.css';
 
 export function Notifications() {
     return (
-        <div className='Notifications' 
-        style={{display: 'flex', marginLeft:"10px"}}>
+        <div className='Notifications' style={{display: 'flex'}}>
             <p>Here is the list of notifications</p>
             <button 
+            style={{marginLeft:"5px"}}
             aria-label='Close' 
             onClick={console.log("Close button has been clicked")}>
             </button>
