06c257a874b34d581f3e9b3d4b92f9127b2b244e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-10 14:29:37 +0300

    Task 3: Creating basic tests

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import logo from './logo.jpg';
+import {getFullYear, getFooterCopy} from './utils.js'
+
+function App(isIndex) {
+  const year = getFullYear();
+  const alx = getFooterCopy(isIndex)
+  return (
+    <>
+    <section>
+      <div className="App-header">
+      <img src={logo} alt="ALX Logo"/>
+      <h1>School dashboard</h1>
+    </div>
+    </section>
+
+    <hr className='Horizontal-rule'></hr>
+
+    <section>
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+     </div>
+     <div className='Login-form'>
+        <form id="signup-form">
+
+        <label htmlFor="email">Email: </label>
+        <input name="email" id="email" placeholder="email@example.com" />
+
+
+        <label htmlFor="password"
+        style={{marginLeft:"20px"}}
+        >Password: </label>
+        <input name="password" id="password" placeholder="*******" />
+
+
+        <button>OK</button>
+      </form>
+
+     </div>
+
+     </section>
+
+     <hr className='Horizontal-rule'></hr>
+
+    <section>  
+    <div className="App-footer">
+        <p>Copyright {year} - {alx}</p>
+      </div> 
+    </section>
+    </>
+
+      );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css'; 
+import App from './App';
+import Notifications from './Notifications';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+const notifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+notifications.render(
+<React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
