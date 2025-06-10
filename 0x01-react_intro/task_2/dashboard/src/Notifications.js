e84babf5502a800cd957fed38b8a5df40e43f729
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-10 13:38:21 +0300

    Notifications requirements

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,6 +1,7 @@
 import './Notifications.css';
 
 export function Notifications() {
+    const getLatestNotification = () => '<strong>Urgent requirement</strong> - complete by EOD';
     return (
         <div className='Notifications' style={{display: 'flex'}}>
             <p>Here is the list of notifications</p>
@@ -8,7 +9,14 @@ export function Notifications() {
             style={{marginLeft:"5px"}}
             aria-label='Close' 
             onClick={console.log("Close button has been clicked")}>
+                <img src='./close-icon.png' alt=''/>
+                X
             </button>
+            <ul>
+                <li data-priority="default">New course available</li> 
+                <li data-priority="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+            </ul>
         </div>
     )
 }
