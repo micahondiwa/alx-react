cbc989417dddae773629440df89c0489e42ab329
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:29:15 +0300

    Cleaned Login

--- 0x02-react_props/task_0/dashboard/src/Login/Login.js
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -13,19 +13,14 @@ export default function Login() {
         <label htmlFor="email">Email: </label>
         <input name="email" id="email" placeholder="email@example.com" />
 
-
         <label htmlFor="password"
         style={{marginLeft:"20px"}}
         >Password: </label>
         <input name="password" id="password" placeholder="*******" />
 
-
         <button>OK</button>
       </form>
-
      </div>
-
     </>
-
       );
 }
