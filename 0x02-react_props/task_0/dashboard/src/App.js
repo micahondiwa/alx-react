ed9c1abbe157a7672bd454dac379c118552dc7ea
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:22:05 +0300

    Section cleanups

--- 0x02-react_props/task_0/dashboard/src/App.js
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -6,22 +6,10 @@ import Footer from './Footer/Footer.js'
 function App(isIndex) {
   return (
     <>
-    <section>
-      <Header/>
-    </section>
-    <Login/>
-    <section>
-
-     </section>
-
-     <hr className='Horizontal-rule'></hr>
-
-    <section>  
-      <Footer/>
-
-    </section>
+    <section> <Header/></section>
+    <section><Login/> </section>
+    <section> <Footer/> </section>
     </>
-
       );
 }
 
