6a6ecb21345c3bad8428e4de2184065099c5ba77
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:30:54 +0300

    Cleaned Footer

--- 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -6,11 +6,8 @@ export default function Footer(isIndex) {
   const year = getFullYear();
   const alx = getFooterCopy(isIndex)
   return (
-    <>
     <div className="App-footer">
         <p>Copyright {year} - {alx}</p>
       </div> 
-    </>
-
       );
 }
