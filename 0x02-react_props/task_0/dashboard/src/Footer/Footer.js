21cb904077389a07c1fa3e1b641c26c2393a3140
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 10:09:55 +0300

    Footer

--- 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -1,1 +1,21 @@
 /**Footerjs */
+import './App.css';
+import logo from './logo.jpg';
+import {getFullYear, getFooterCopy} from './utils.js'
+
+function App(isIndex) {
+  const year = getFullYear();
+  const alx = getFooterCopy(isIndex)
+  return (
+    <>
+    <section>  
+    <div className="App-footer">
+        <p>Copyright {year} - {alx}</p>
+      </div> 
+    </section>
+    </>
+
+      );
+}
+
+export default App;
