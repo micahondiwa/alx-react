75f27b9f363fcadf630b09f288e35df084824fe0
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 10:06:04 +0300

    Headerjs

--- 0x02-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -1,1 +1,20 @@
 /**Header js */
+import './App.css';
+import logo from './src/logo.jpg';
+
+function Header() {
+  return (
+    <>
+    <section>
+      <div className="App-header">
+      <img src={logo} alt="ALX Logo"/>
+      <h1>School dashboard</h1>
+    </div>
+    </section>
+
+    <hr className='Horizontal-rule'></hr>
+    </> 
+    )
+};
+
+export default App;
