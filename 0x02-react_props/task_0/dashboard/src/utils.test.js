b6cb2df77210437d2b502f72edf7b21b92f79a58
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 12:01:53 +0300

    Test files

--- 0x02-react_props/task_0/dashboard/src/utils.test.js
+++ 0x02-react_props/task_0/dashboard/src/utils.test.js
@@ -1,24 +1,18 @@
-/**Test for utils js */
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
 
-import {getFullYear, getFooterCopy, getLatestNotification} from './utils'
+test('getFullYear returns the current year', () => {
+  const currentYear = new Date().getFullYear();
+  expect(getFullYear()).toBe(currentYear);
+});
 
-describe('utils.js tests', () => {
-    test('getFullYear returns the current year', () => {
-        const currentYear = new Date().getFullYear();
-        expect(getFullYear().toBe(currentYear));
-    });
+test('getFooterCopy returns "ALX" when isIndex is true', () => {
+  expect(getFooterCopy(true)).toBe("ALX");
+});
 
-    test('getFooterCopy returns "ALX" when isIndex is true', () => {
-        expect(getFooterCopy(true).toBe("ALX"));
-    });
+test('getFooterCopy returns "ALX main dashboard" when isIndex is false', () => {
+  expect(getFooterCopy(false)).toBe("ALX main dashboard");
+});
 
-    test('getFooterCopy returns "ALX main dashboard" when isIndex is false', () => {
-        expect(getFooterCopy(false).toBe("ALX main dashboard"));
-    });
-
-    test('getLatestNotification returns correct HTML string', () => {
-        expect(getLatestNotification()).toBe(
-            "<strong>Urgent requirement</strong> - complete by EOD"
-        );
-    });
+test('getLatestNotification returns correct HTML string', () => {
+  expect(getLatestNotification()).toContain('<strong>Urgent requirement</strong>');
 });
