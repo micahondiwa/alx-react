4c54afc5a93ed03faefda6ea2140bcd0a9fb384b
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 12:50:53 +0300

    Import Footer

--- 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
+++ 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
@@ -1,1 +1,16 @@
 /**Footerjs test */
+
+import { shallow } from 'enzyme';
+import Footer from './Footer';
+
+describe('Footer component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders text containing "Copyright"', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.text().toLowerCase()).toContain('copyright');
+  });
+});
--- 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
+++ 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
@@ -1,1 +1,16 @@
 /**Code Loginjs test */
+import { shallow } from 'enzyme';
+import Login from './Login';
+
+describe('Login component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('contains 2 input tags and 2 label tags', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find('input').length).toBe(2);
+    expect(wrapper.find('label').length).toBe(2);
+  });
+});
