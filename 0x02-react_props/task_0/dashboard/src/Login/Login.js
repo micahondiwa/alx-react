243f48088abbf4925229b4434a11bfb1786ed0c7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 10:14:32 +0300

    App login

--- 0x02-react_props/task_0/dashboard/src/Login/Login.js
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -1,1 +1,33 @@
 /**Loginjs */
+import './Login.css';
+
+function Login(isIndex) {
+  return (
+    <>
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+     </div>
+     <div className='Login-form'>
+        <form id="signup-form">
+
+        <label htmlFor="email">Email: </label>
+        <input name="email" id="email" placeholder="email@example.com" />
+
+
+        <label htmlFor="password"
+        style={{marginLeft:"20px"}}
+        >Password: </label>
+        <input name="password" id="password" placeholder="*******" />
+
+
+        <button>OK</button>
+      </form>
+
+     </div>
+
+    </>
+
+      );
+}
+
+export default Login;
