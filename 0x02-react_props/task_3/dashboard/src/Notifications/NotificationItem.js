90bb2e0cf7a3b3ea45571b5d2ede195c6b3ddec1
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 16:33:48 +0300

    NotificationItems

--- 0x02-react_props/task_3/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -1,9 +1,13 @@
 /**Notificationitem */
+import React from "react";
 
 export default function Notificationitem() {
     return(
         <>
-        <li type=" data-notification-type" value ="<li/>"  html="dangerouslySetInnerHTML"></li>
+        <li>
+        type=<data-notification-type/> value <li/>  html=<dangerouslySetInnerHTML/>
+        <li style={{color: 'blue'}} data-priority="default">New course available</li> 
+        </li>
         </>
-    )
-}
+    )};
+
