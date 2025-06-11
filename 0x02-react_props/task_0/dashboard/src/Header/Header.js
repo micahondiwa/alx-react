6aaf9f76fefaaf7499cc49bde8fa07f2d57d936f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:12:51 +0300

    Remove hr from Header.js

--- 0x02-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -5,14 +5,10 @@ import logo from './logo.jpg';
 export default function Header() {
   return (
     <>
-    <section>
       <div className="App-header">
       <img src={logo} alt="ALX Logo"/>
       <h1>School dashboard</h1>
     </div>
-    </section>
-
-    <hr className='Horizontal-rule'></hr>
     </> 
     )
 };
