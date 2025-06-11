2d3c4c8acda439bfa0d03e6e3c234c3f21c1e790
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:10:52 +0300

    Import login

--- 0x02-react_props/task_0/dashboard/src/App.js
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -11,28 +11,8 @@ function App(isIndex) {
     <section>
       <Header/>
     </section>
-
+    <Login/>
     <section>
-    <div className="App-body">
-      <p>Login to access the full dashboard</p>
-     </div>
-     <div className='Login-form'>
-        <form id="signup-form">
-
-        <label htmlFor="email">Email: </label>
-        <input name="email" id="email" placeholder="email@example.com" />
-
-
-        <label htmlFor="password"
-        style={{marginLeft:"20px"}}
-        >Password: </label>
-        <input name="password" id="password" placeholder="*******" />
-
-
-        <button>OK</button>
-      </form>
-
-     </div>
 
      </section>
 
