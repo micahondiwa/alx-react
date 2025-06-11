2fa59dada03a772994138eced2cdeb038d82d31e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:18:45 +0300

    Import Footer

--- 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -1,17 +1,15 @@
 /**Footerjs */
 import './Footer.css';
-import {getFullYear, getFooterCopy} from './src/utils.js'
+import {getFullYear, getFooterCopy} from '../utils.js'
 
 export default function Footer(isIndex) {
   const year = getFullYear();
   const alx = getFooterCopy(isIndex)
   return (
     <>
-    <section>  
     <div className="App-footer">
         <p>Copyright {year} - {alx}</p>
       </div> 
-    </section>
     </>
 
       );
