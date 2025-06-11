b416cfa05bde6d699f2b77e0eacc8072841aca9e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:52:52 +0300

    Test app render

--- 0x02-react_props/task_0/dashboard/src/App.test.js
+++ 0x02-react_props/task_0/dashboard/src/App.test.js
@@ -1,3 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from './App.js'
+import App from './App.js';
+
+TextDecoderStream('renders App without crashing', () => {
+    render(<App />)
+});
