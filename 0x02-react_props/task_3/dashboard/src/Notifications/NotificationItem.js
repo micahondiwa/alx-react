097fd2069fdca53f7aeed3c41b571cb1738c436c
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 16:53:35 +0300

    NotificationItems

--- 0x02-react_props/task_3/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -5,10 +5,22 @@ import PropTypes from prop-PropTypes;
 export default function Notificationitem({type, html, value}) {
     return(
         <>
-        <li>
-        ata-notification-type={type} value 
-
-        </li>
+        <li
+        ata-notification-type={type} {...Notificationitem(html ? {dangerouslySetInnerHTML: html}: {})}/> {!html && value}
         </>
     )};
+
+Notificationitem.PropTypes={
+    type: PropTypes.string,
+    value: PropTypes.string,
+    html: PropTypes.Shape({
+        __html: PropTypes.string, 
+    }),
+};
+
+Notificationitem.defaultProps = {
+    type: 'default',
+    value: '', 
+    html: null
+};
 
