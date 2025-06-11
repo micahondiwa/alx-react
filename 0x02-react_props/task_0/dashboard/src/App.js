12c9d4c786f6622da604db211c0853fefb09c5aa
micahondiwa <micah.oketch.ondiwa@gmail.com>
2025-06-11 11:42:26 +0300

    Import notifications

--- 0x02-react_props/task_0/dashboard/src/App.js
+++ 0x02-react_props/task_0/dashboard/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Header from './Header/Header.js'
-import Login from './Login/Login.js'
-import Footer from './Footer/Footer.js'
-import Notifications from './Notifications.js'
+import Notifications from './Notifications.js';
+import Login from './Login/Login.js';
+import Footer from './Footer/Footer.js';
+import Header from './Header/Header.js';
 
 function App() {
   return (
